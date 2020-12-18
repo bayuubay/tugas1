e9949a17a27e5ee596ddc35d11d2e7b4c3875073
bayuubay <bayu.suryo.a@gmail.com>
2020-12-18 15:18:17 +0700

    menambahkan file

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ const app = express();
 const port = 5000;
 
 app.get('/', function(req, res){
-  res.send('Hello My name is YOUR_NAME');
+  res.send('Hello My name is Bayu from GET');
 });
 
+app.post("/", function (req, res) {
+  res.send("Hello My name is Bayu but it from POST");
+});
+
+app.put("/", function (req, res) {
+  res.send("Hello My name is Bayu but it from PUT");
+});
+
+app.patch("/", function (req, res) {
+  res.send("Hello My name is Bayu but it from PATCH");
+});
+
+app.delete("/", function (req, res) {
+  res.send("Hello My name is Bayu but it from DELETE");
+});
 app.listen(port, function(){
   console.log('Hello my apps is running on port '+port);
 });
